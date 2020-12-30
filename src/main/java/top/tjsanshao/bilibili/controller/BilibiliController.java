cce41ddae786272aca96ae9577feb4f427e7f19a
TjSanshao <1305705188@qq.com>
2020-12-30 10:37:51 +0800

    test pass

--- src/main/java/top/tjsanshao/bilibili/controller/BilibiliController.java
+++ src/main/java/top/tjsanshao/bilibili/controller/BilibiliController.java
@@ -2,8 +2,10 @@ package top.tjsanshao.bilibili.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.tjsanshao.bilibili.action.TaskStatus;
 import top.tjsanshao.bilibili.action.UserCheck;
 import top.tjsanshao.bilibili.api.OftenAPI;
+import top.tjsanshao.bilibili.current.CurrentUser;
 
 import javax.annotation.Resource;
 
@@ -20,11 +22,27 @@ public class BilibiliController {
 
     @Resource
     private UserCheck userCheck;
+    @Resource
+    private TaskStatus taskStatus;
+
+    @Resource
+    private CurrentUser currentUser;
 
     @RequestMapping("/login")
     public String login() {
         userCheck.act();
         return "success";
+    }
+
+    @RequestMapping("/taskStatus")
+    public String taskStatus() {
+        taskStatus.act();
+        return "success";
+    }
+
+    @RequestMapping("/current")
+    public CurrentUser current() {
+        return currentUser;
     }
 
     @RequestMapping("/coinBalance")
